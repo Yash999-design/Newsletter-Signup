19e1f8ab274e2ab8f5d6f9a18030fcd22f750f58
Yash Pandey <knowledge86400@gmail.com>
2021-12-15 11:17:26 +0530

    Initial Commit

---
+++ app.js
@@ -0,0 +1,32 @@
+//jshint esversion: 6
+
+const express = require("express");
+const bodyParser = require("body-parser");
+const request = require("request");
+
+const app = express();
+
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/signup.html");
+});
+
+app.post("/", (req, res) => {
+  var firstName = req.body.fName;
+  var lastName = req.body.lName;
+  var email = req.body.email;
+
+  console.log(firstName, lastName, email);
+});
+
+app.listen(3000, () => {
+  console.log("Server is running on port 3000.");
+});
+
+
+/* 
+API Key
+cb9bc060f0108c9ad4f1481afdf78b67-us20
+*/
